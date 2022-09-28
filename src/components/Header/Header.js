ef113dbbc4116f0202afc41cd68dbb3f3fcd8a0e
ikhlas5 <mohammadikhlas765@gmail.com>
2022-09-28 23:29:05 +0600

    second commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import'./Header.css'
+const Header = () => {
+    
+    return (
+        <div className='header'>
+            <div>
+                <img src="1048219_OL3O6X0.jpg"alt="" />
+            </div>
+            <div>
+
+            <h1>Our <span className='header-color'>Gym Center</span></h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
